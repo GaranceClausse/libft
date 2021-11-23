89df349498984c945338c2c432acf63e27dd092b
Garance CLAUSSE <gclausse@e1r3p18.42.fr>
2021-11-23 16:19:20 +0100

    all the functions done but not tested

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 16:03:32 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/23 16:11:02 by gclausse         ###   ########.fr       */
+/*   Created: 2021/11/23 15:53:59 by gclausse          #+#    #+#             */
+/*   Updated: 2021/11/23 16:18:53 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bzero(void *s, size_t n)
-{
-	char	*s2;
-	size_t	i;
+#include <stdlib.h>
 
-	s2 = s;
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	i;
+	char	*cpy;
+
 	i = 0;
-	while (i < n)
+	cpy = malloc(sizeof(size) * (count));
+	if (!cpy)
+		return (NULL);
+	while (cpy[i])
 	{
-		s2[i] = '\0';
+		cpy[i] = 0;
 		i++;
 	}
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -1,25 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 16:03:32 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/23 16:11:02 by gclausse         ###   ########.fr       */
+/*   Created: 2021/11/23 15:48:37 by gclausse          #+#    #+#             */
+/*   Updated: 2021/11/23 15:53:44 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bzero(void *s, size_t n)
-{
-	char	*s2;
-	size_t	i;
+#include <stdlib.h>
 
-	s2 = s;
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	int		len;
+	char	*cpy;
+
 	i = 0;
-	while (i < n)
+	len = 0;
+	while (s1[len])
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+	if (!cpy)
+		return (NULL);
+	while (s1[i])
 	{
-		s2[i] = '\0';
+		cpy[i] = s1[i];
 		i++;
 	}
+	cpy[i] = '\0';
+	return (cpy);
 }
