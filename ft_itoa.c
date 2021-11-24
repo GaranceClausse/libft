b4c1865d5677f9d1ed840662600f5a4a1c2810a7
Garance CLAUSSE <gclausse@e2r9p11.clusters.42paris.fr>
2021-11-24 15:25:30 +0100

    itoa + .h done

---
+++ ft_itoa.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 15:03:08 by gclausse          #+#    #+#             */
+/*   Updated: 2021/11/24 15:24:39 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_itoa(int n)
+{
+	int	i;
+	int	cpt;
+	int	nbr;
+	char	*str;
+
+	i = 0;
+	cpt = 1;
+	nbr = n;
+	if (n < 0)
+	{
+		nbr = nbr * -1;
+		cpt = 2;
+	}
+	while (nbr % 10)
+	{
+		cpt++;
+		nbr = nbr / 10;
+	}
+	str = malloc(sizeof(char) * (cpt + 1));
+	if (!str)
+		return (NULL);
+	str[cpt] = '\0';
+	if (n < 0)
+		str[0] = '-';
+	while ((cpt + 1) > 0)
+	{
+		str[cpt - 1] = n % 10 + 48;
+		n = n / 10;
+		cpt--;
+	}
+	return (str);
+}
