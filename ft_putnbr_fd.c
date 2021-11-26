5606f5cbe8b742b93e0f62c4d8e04f08f18bd5b9
Garance CLAUSSE <gclausse@e1r6p11.42.fr>
2021-11-26 16:19:21 +0100

    split almost ok?

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,27 +6,20 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:40:20 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/25 12:43:26 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/26 14:38:31 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
-{
-	write (fd, &c, 1);
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	int			i;
 	long int	nbr;
 
-	i = 0;
 	nbr = n;
-	if (nbr < 0)
+	if (n < 0)
 	{
-		write(fd, "-", 1);
+		ft_putchar_fd('-', fd);
 		nbr = nbr * -1;
 	}
 	if (nbr < 10)
@@ -34,6 +27,6 @@ void	ft_putnbr_fd(int n, int fd)
 	else
 	{
 		ft_putnbr_fd(nbr / 10, fd);
-		ft_putchar_fd((nbr % 10), fd);
+		ft_putchar_fd((nbr % 10) + 48, fd);
 	}
 }
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:27:26 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/25 12:33:33 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/26 15:59:16 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,17 @@ int	num_of_sep(const char *str, char c)
 	while (str[i])
 	{
 		if (str[i] == c)
+		{
 			cpt++;
+			while (str[i] == c)
+				i++;
+		}
 		i++;
 	}
+	if (str[i - 1] == c)
+		cpt--;
 	return (cpt);
+	printf("cpt = %d\n", cpt);
 }
 
 char	**ft_split(char const *s, char c)
@@ -37,22 +44,42 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	j = 0;
-	cpy = malloc(sizeof(char) * (num_of_sep(s, c) + 1));
+	printf("string : %s\n", s);
+	printf("num of sep %d\n", (num_of_sep(s, c)));
+	cpy = malloc(sizeof(char *) * (num_of_sep(s, c)));
 	while (s[i])
 	{
-		k = ft_strlen(ft_strchr(&s[i], c));
-		cpy[j] = malloc(sizeof(char *) * (ft_strlen(&s[i]) - k));
-		k = 0;
-		while (s[i] != c)
-		{			
-			cpy[j][k] = s[i];
+		if (s[i] != c)
+		{
+			cpy[j] = malloc(sizeof(char) * (ft_strlen(&s[i]) - ft_strlen(ft_strchr(&s[i], c)) + 1));
+			k = 0;
+			while (s[i] != c && s[i])
+			{			
+				cpy[j][k] = s[i];
+				i++;
+				k++;
+			}
+			cpy[j][k] = '\0';
+			printf("cpy = %s\n", cpy[j]);
+			j++;
 			i++;
-			k++;
 		}
-		cpy[j][k] = '\0';
-		j++;
 		i++;
 	}
-	cpy[j][k] = '\0';
 	return (cpy);
 }
+/*
+int	main()
+{
+	char **tab = ft_split("tripouille42 745i 4 poii4", '4');
+
+	printf("0 = %s\n", tab[0]);
+	printf("1 = %s\n", tab[1]);
+	printf("2 = %s\n", tab[2]);
+	printf("3 = %s\n", tab[3]);
+
+}*/
+
+
+
+
