2acbb095b01d8f8cd77e3fae430df51556c3021f
Garance CLAUSSE <gclausse@e1r6p11.42.fr>
2021-11-26 19:20:30 +0100

    split working tommorow??

--- ft_split.c
+++ ft_split.c
@@ -6,80 +6,99 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:27:26 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/26 15:59:16 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/26 19:19:10 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	num_of_sep(const char *str, char c)
+int	numlet(const char *str, char c)
 {
 	int	i;
 	int	cpt;
 
 	i = 0;
 	cpt = 0;
+	if (c == '\0')
+		return (ft_strlen(str));
+	while (str[i] != c && str[i])
+	{
+		i++;
+		cpt++;
+	}
+	return (cpt);
+}
+
+int	num_of_word(const char *str, char c)
+{
+	int	i;
+	int	cpt;
+
+	i = 0;
+	cpt = 0;
+	if (c == '\0')
+		return (0);
 	while (str[i])
 	{
-		if (str[i] == c)
-		{
+		while (str[i + 1] == c)
+			i++;
+		if (str[i] == c && str[i + 1] != c && str[i + 1] != '\0')
 			cpt++;
-			while (str[i] == c)
-				i++;
-		}
 		i++;
 	}
-	if (str[i - 1] == c)
-		cpt--;
 	return (cpt);
-	printf("cpt = %d\n", cpt);
+
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**cpy;
-	int		i;
+	size_t		i;
 	int		j;
 	int		k;
+	char	**cpy;
 
+	cpy = malloc(sizeof(char *) * (num_of_word(s, c) + 1));
+	printf("cpt : %d\n", (num_of_word(s, c) + 1));
+	if (!cpy)
+		return (NULL);
 	i = 0;
 	j = 0;
-	printf("string : %s\n", s);
-	printf("num of sep %d\n", (num_of_sep(s, c)));
-	cpy = malloc(sizeof(char *) * (num_of_sep(s, c)));
-	while (s[i])
+	while (i <= ft_strlen(s))
 	{
-		if (s[i] != c)
+		k = 0;
+		if (s[i] != c && s[i])
 		{
-			cpy[j] = malloc(sizeof(char) * (ft_strlen(&s[i]) - ft_strlen(ft_strchr(&s[i], c)) + 1));
-			k = 0;
-			while (s[i] != c && s[i])
-			{			
+			cpy[j] = malloc(sizeof(char) * (numlet(&s[i], c) + 1));
+			printf("num letters : %d\n", (numlet(&s[i], c) + 1));
+			while (s[i] != c && s[i + 1])
+			{
 				cpy[j][k] = s[i];
 				i++;
 				k++;
 			}
 			cpy[j][k] = '\0';
-			printf("cpy = %s\n", cpy[j]);
+			printf("cpy : %s\n\n", cpy[j]);
 			j++;
-			i++;
+			printf(" j = %d\n", j);
 		}
 		i++;
 	}
+	cpy[j] = NULL;
 	return (cpy);
 }
 /*
 int	main()
 {
-	char **tab = ft_split("tripouille42 745i 4 poii4", '4');
+	char **tab = ft_split("tripouille     tr   rtb   ", '\0');
+
 
 	printf("0 = %s\n", tab[0]);
 	printf("1 = %s\n", tab[1]);
 	printf("2 = %s\n", tab[2]);
 	printf("3 = %s\n", tab[3]);
 
-}*/
-
+}
+*/
 
 
 
