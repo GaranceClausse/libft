420dd479e94b87617dbfe4d60ead2850e4f8d556
Garance CLAUSSE <gclausse@e1r12p20.42.fr>
2021-11-27 18:37:41 +0100

    memory error corrected on trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:26:05 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/27 17:56:59 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/27 18:37:14 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	j = ft_strlen(s1);
 	str = (char *)s1;
 	sep = (char *)set;
-	while (find_ocurence(sep, str[i]) == 1)
+	while (find_ocurence(sep, str[i]) == 1 && str[i])
 		i++;
-	while (find_ocurence(sep, str[j - 1]) == 1)
+	while (find_ocurence(sep, str[j - 1]) == 1 && j > i)
 		j--;
 	return (ft_substr(str, i, (j - i)));
 }
