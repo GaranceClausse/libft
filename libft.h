a6eaa25f5896d2eb014cd6e4f5bdc39436c3d56d
Garance CLAUSSE <gclausse@e1r12p20.42.fr>
2021-11-27 20:58:02 +0100

    lib

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:55:41 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/25 14:14:11 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/27 15:27:55 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H_ 
@@ -37,7 +37,7 @@ char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
-void	*ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
