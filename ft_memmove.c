69d93c699daae688cfa37ff0146c67c637cc6dc2
Garance CLAUSSE <gclausse@e1r6p11.42.fr>
2021-11-29 11:34:44 +0100

    memmove debug

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 14:51:30 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/26 12:16:18 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/29 11:33:18 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,33 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	i = 0;
 	csrc = (char *)src;
 	move = (char *)dst;
-	while (i < len)
+	if (!src || !dst)
+		return (0);
+	if (src < dst && dst < src + len)
 	{
-		move[i] = csrc[i];
-		i++;
+		while (len--)
+		{
+			move[len] = csrc[len];
+		}
 	}
-	i = 0;
+	else
+	{
+		while (i < len)
+		{
+			move[i] = csrc[i];
+			i++;
+		}
+	}
 	return (move);
 }
+/*
+int	main()
+{
+	char	src[] = "lorem ipsum dolor sit amet";
+	char	*dest;
+
+	dest = src + 1;
+	printf("vrai %s\n", memmove(dest, src, 8));
+	printf("moi %s\n", ft_memmove(dest, src, 8));
+
+}*/
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:27:26 by gclausse          #+#    #+#             */
-/*   Updated: 2021/11/27 17:19:35 by gclausse         ###   ########.fr       */
+/*   Updated: 2021/11/27 20:57:06 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,46 +38,90 @@ int	num_of_word(const char *str, char c)
 	cpt = 0;
 	if (c == '\0')
 		return (0);
+	while (str[i] == c)
+		i++;
 	while (str[i])
 	{
-		while (str[i + 1] == c)
-			i++;
-		if (str[i] == c && str[i + 1] != c && str[i + 1] != '\0')
-			cpt++;
+		if (str[i] == c)
+		{
+			while (str[i] == c)
+			{
+				if (str[i + 1] != c || str[i + 1] == '\0')
+					cpt++;
+				i++;
+			}
+		}
 		i++;
 	}
 	return (cpt);
+}
+
+char	*create_word(const char *s, char c)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = malloc(sizeof(char) * (numlet(&s[i], c) + 1));
+	if (!str)
+		return (NULL);
+	str[0] = '\0';
+	while (s[i] != c && s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	int		j;
-	int		k;
 	char	**cpy;
 
 	cpy = malloc(sizeof(char *) * (num_of_word(s, c) + 1));
 	if (!cpy)
 		return (NULL);
+	printf("num of words :%d\n", (num_of_word(s, c) + 1));
+
+	cpy[0] = NULL;
 	i = 0;
 	j = 0;
-	while (i <= ft_strlen(s))
+	while (s[i] && j <= num_of_word(s, c))
 	{
-		k = 0;
 		if (s[i] != c && s[i])
 		{
-			cpy[j] = malloc(sizeof(char) * (numlet(&s[i], c) + 1));
-			while (s[i] != c && s[i])
-			{
-				cpy[j][k] = s[i];
-				i++;
-				k++;
-			}
-			cpy[j][k] = '\0';
+			cpy[j] = create_word(&s[i], c);
 			j++;
 		}
-		i++;
+
+		i = i + (numlet(&s[i], c)) + 1;
 	}
 	cpy[j] = NULL;
 	return (cpy);
+}
+
+int main()
+{
+	char **tab = ft_split("loremipsum dolor sit amet,        consectetur        ipiscing et.    ", ' ');
+	
+	printf("%s\n", tab[0]);
+printf("%s\n", tab[1]);
+printf("%s\n", tab[2]);
+
+printf("%s\n", tab[3]);
+printf("%s\n", tab[4]);
+printf("%s\n", tab[5]);
+printf("%s\n", tab[6]);
+printf("%s\n", tab[7]);
+
+printf("%s\n", tab[8]);
+printf("%s\n", tab[9]);
+printf("%s\n", tab[10]);
+printf("%s\n", tab[11]);
+printf("%s\n", tab[12]);
+
+
 }
